5631b45c4328f0660b09138a8fb8ead56744519f
Jared Holgate <jaredfholgate@gmail.com>
2021-01-11 20:05:51 +0000

    Add RSS feed

--- Blog.UI/Controllers/HomeController.cs
+++ Blog.UI/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Net.Mime;
+using System.ServiceModel.Syndication;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 using Blog.Data;
 using Blog.Service;
 using Blog.UI.Models;
@@ -23,12 +24,48 @@ namespace Blog.UI.Controllers
       _blogService = blogService;
     }
 
-    [AllowAnonymous]
     public IActionResult Index()
     {
       var articles = _blogService.GetArticles();
       var articleSummary = GetArticleSummaryViewModel(articles);
       return View(articleSummary);
+    }
+
+    [Route("rss")]
+    [ResponseCache(Duration = 1200)]
+    [HttpGet]
+    public IActionResult Rss()
+    {
+      var feed = new SyndicationFeed("Jared Holgate Blog", "Jared Holgate's blog articles.", new Uri("https://jaredholgate.co.uk/rss"), "RSSUrl", DateTime.Now)
+      {
+        Copyright = new TextSyndicationContent($"{DateTime.Now.Year} Jared Holgate")
+      };
+
+      var items = new List<SyndicationItem>();
+      var postings = _blogService.GetArticles();
+      foreach (var item in postings)
+      {
+        var postUrl = Url.Action("Article", "Read", new { id = item.UrlTitle }, HttpContext.Request.Scheme);
+        var title = item.Title;
+        var description = item.Summary;
+        items.Add(new SyndicationItem(title, description, new Uri(postUrl), item.UrlTitle, item.Date));
+      }
+      feed.Items = items;
+      var settings = new XmlWriterSettings
+      {
+        Encoding = Encoding.UTF8,
+        NewLineHandling = NewLineHandling.Entitize,
+        NewLineOnAttributes = true,
+        Indent = true
+      };
+      using var stream = new MemoryStream();
+      using (var xmlWriter = XmlWriter.Create(stream, settings))
+      {
+        var rssFormatter = new Rss20FeedFormatter(feed, false);
+        rssFormatter.WriteTo(xmlWriter);
+        xmlWriter.Flush();
+      }
+      return File(stream.ToArray(), "application/rss+xml; charset=utf-8");
     }
 
     private static List<ArticleSummaryViewModel> GetArticleSummaryViewModel(IEnumerable<Article> articles)
@@ -82,13 +119,18 @@ namespace Blog.UI.Controllers
   <changefreq>daily</changefreq>
   <priority>0.90</priority>
 </url>
+<url>
+  <loc>https://www.jaredholgate.co.uk/rss</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.90</priority>
+</url>
 {0}
 </urlset>";
 
 
       var articles = _blogService.GetArticles();
       var articleNodes = articles.Select(o => $@"<url>
-  <loc>https://www.jaredholgate.co.uk/Blog/{o.Title.Replace(" ","-")}</loc>
+  <loc>https://www.jaredholgate.co.uk/Blog/{o.UrlTitle}</loc>
   <changefreq>daily</changefreq>
   <priority>0.80</priority>
 </url>");
